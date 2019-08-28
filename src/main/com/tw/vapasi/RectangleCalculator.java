688b51a6362f913ddffb152ce8a7a95b4e6de448
Merlin <merlin.p.varghese@gmail.com>
2019-08-28 12:30:44 +0530

    [Merlin,Randeep]Add. Second test case and code refactored

--- src/main/com/tw/vapasi/RectangleCalculator.java
+++ src/main/com/tw/vapasi/RectangleCalculator.java
@@ -4,7 +4,16 @@ package com.tw.vapasi;
  * Understand a figure with four straight sides and four right angles, with unequal adjacent sides.
  */
 public class RectangleCalculator {
-    public int calculateArea(int length, int width) {
-        return 20;
+    private int length;
+    private int width;
+
+    public RectangleCalculator(int length, int width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int calculateArea() {
+        return length * width;
     }
 }
--- src/test/RectangleCalculatorTest.java
+++ src/test/RectangleCalculatorTest.java
@@ -6,7 +6,13 @@ class RectangleCalculatorTest {
 
     @Test
     void expect20ForLength5AndWidth4() {
-        RectangleCalculator calculator = new RectangleCalculator();
-        Assertions.assertEquals(20, calculator.calculateArea(5, 4));
+        RectangleCalculator calculator = new RectangleCalculator(5, 4);
+        Assertions.assertEquals(20, calculator.calculateArea());
+    }
+
+    @Test
+    void expect12ForLength4AndWidth3() {
+        RectangleCalculator calculator = new RectangleCalculator(4, 3 );
+        Assertions.assertEquals(12, calculator.calculateArea());
     }
 }
